45ebd1f6ad61cd00e634af8ba19c8c9357ea59fb
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-01-27 19:50:25 -0600

    Add image to start body section

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,16 @@
+import './style.css';
+import Icon from './coffee_ultimate.jpeg';
+
 const loadPage = () => {
     const contentDiv = document.querySelector('#content');
     const coffeeHeader = document.createElement('h1');
     coffeeHeader.innerHTML = "Big Rob's Coffee Shop";
     contentDiv.appendChild(coffeeHeader);
+
+    // Add the image to our existing content
+    // const coffeeImage = new Image();
+    // coffeeImage.src = Icon;
+    // contentDiv.appendChild(coffeeImage);
 
     const newDiv = document.createElement('div');
     newDiv.id = 'pageInfo';
@@ -10,6 +18,6 @@ const loadPage = () => {
     contentDiv.appendChild(newDiv);
 
     return contentDiv;
-} 
+}
 
-export {loadPage};
+export { loadPage };
