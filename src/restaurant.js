408ac02919ea20c12fdafa1db9e520b98a310c1a
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-01-27 21:22:13 -0600

    Style navbar and add img for about section

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,21 +1,27 @@
 import './style.css';
 import Icon from './coffee_ultimate.jpeg';
+import Icon2 from './coffee-feel.jpeg';
 
 const loadPage = () => {
     const contentDiv = document.querySelector('#content');
-    const coffeeHeader = document.createElement('h1');
-    coffeeHeader.innerHTML = "Big Rob's Coffee Shop";
-    contentDiv.appendChild(coffeeHeader);
 
     // Add the image to our existing content
-    // const coffeeImage = new Image();
-    // coffeeImage.src = Icon;
-    // contentDiv.appendChild(coffeeImage);
+    const coffeeImage = new Image();
+    coffeeImage.src = Icon;
+    contentDiv.appendChild(coffeeImage);
 
     const newDiv = document.createElement('div');
     newDiv.id = 'pageInfo';
-    newDiv.innerHTML = "Big Rob's coffee shop opened its doors in 2037"
     contentDiv.appendChild(newDiv);
+
+    const coffeeInfo = document.createElement('p');
+    coffeeInfo.innerHTML = "Do you feel like you can't get through the day without enough coffee? Do you just like the taste of coffee? Or do you perhaps just like to grab coffee with a friend and happen to be in Portland, OR? If any of these are true then you should stop by Big Rob's Coffeeshop."
+    newDiv.appendChild(coffeeInfo);
+
+
+    const coffeeImage2 = new Image();
+    coffeeImage2.src = Icon2;
+    contentDiv.appendChild(coffeeImage2);
 
     return contentDiv;
 }
